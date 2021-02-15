7c133d704eb938864ca20583b017973e57bb4881
jblam <jeremy.blamey@gmail.com>
2021-02-15 14:53:05 +1100

    Attempt to nicely read incomplete lines.
    Too tedious to continue with. Possibly MemoryStream is the jerk here?

---
+++ Multiflash.Tests/SerialRedirectionBehaviour.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace JBlam.Multiflash.Tests
+{
+    [TestClass]
+    public class SerialRedirectionBehaviour
+    {
+        [TestMethod]
+        public async Task CanYieldReturnLines()
+        {
+            var stream = new MemoryStream();
+            var reader = new StreamReader(stream);
+            var writer = new StreamWriter(stream);
+            var list = new List<string>();
+            void Append(string s)
+            {
+                var oldPosition = stream.Position;
+                writer.Write(s);
+                writer.Flush();
+                stream.Position = oldPosition;
+            }
+
+            CancellationTokenSource cancellationTokenSource = new();
+
+            var e = Serial.SerialConnection.ConsumeAsync(reader, list, cancellationTokenSource.Token);
+            var outputTask = e.ToListAsync();
+            Append("Line one\n");
+            Append("Line ");
+            Append("two\n");
+            await Task.Yield();
+            cancellationTokenSource.Cancel();
+            writer.Close();
+            var result = await outputTask;
+#pragma warning disable CA2012 // Use ValueTasks correctly
+            // Justification: I am *literally* doing as the hint-tip says, and asserting `IsCompleted`.
+#pragma warning restore CA2012 // Use ValueTasks correctly
+            //Assert.IsTrue(outputTask.IsCompleted);
+            //CollectionAssert.AreEqual(new[] { "Line one\n", "Line two\n" }, outputTask.Result);
+        }
+    }
+}
---
+++ Multiflash/Serial/SerialConnection.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace JBlam.Multiflash.Serial
+{
+    class SerialConnection
+    {
+        public static SerialConnection Open(string comPort, int baud)
+        {
+            var port = new SerialPort(comPort)
+            {
+                BaudRate = baud
+            };
+            port.Open();
+            return new SerialConnection(port);
+        }
+        SerialConnection(SerialPort port)
+        {
+            this.port = port;
+            var reader = new StreamReader(port.BaseStream, Encoding.UTF8);
+            Lines = ConsumeAsync(reader, Output, EnumeratorCancellationSource.Token);
+        }
+        internal static async Task<int?> TryReadAsync(StreamReader reader, Memory<char> buffer, CancellationToken token)
+        {
+            if (token.IsCancellationRequested)
+                return null;
+            try
+            {
+                return await reader.ReadAsync(buffer, token);
+            }
+            catch (ObjectDisposedException)
+            {
+                return null;
+            }
+            catch (TaskCanceledException)
+            {
+                return null;
+            }
+        }
+        internal static async IAsyncEnumerable<string> ConsumeAsync(StreamReader reader, IList<string> fullLines, [EnumeratorCancellation]CancellationToken token)
+        {
+            var buffer = new char[1024].AsMemory();
+            // TODO: how to detect end of stream then?
+            while (true)
+            {
+                await Task.Yield();
+                var length = await TryReadAsync(reader, buffer, token).ConfigureAwait(false);
+                if (!length.HasValue)
+                    yield break;
+                var section = buffer[..length.Value];
+                while (section.Length > 0)
+                {
+                    var nextNewline = section.Span.IndexOf('\n');
+                    var hasNewline = nextNewline >= 0;
+                    var end = hasNewline ? nextNewline + 1 : section.Length;
+                    var substring = section[0..end];
+                    section = section[end..];
+                    string yieldLine;
+                    if (fullLines.Count == 0 || fullLines[^1].EndsWith('\n'))
+                    {
+                        yieldLine = substring.ToString();
+                        fullLines.Add(yieldLine);
+                    }
+                    else
+                    {
+                        yieldLine = fullLines[^1] + substring;
+                        fullLines[^1] = yieldLine;
+                    }
+                    if (hasNewline)
+                    {
+                        yield return yieldLine;
+                    }
+                }
+            }
+        }
+
+        readonly SerialPort port;
+
+        public IAsyncEnumerable<string> Lines { get; }
+
+        /// <summary>
+        /// Gets an observable collection of the output in lines, including incomplete
+        /// lines.
+        /// </summary>
+        public ObservableCollection<string> Output { get; } = new();
+        public CancellationTokenSource EnumeratorCancellationSource { get; } = new();
+
+    }
+}
