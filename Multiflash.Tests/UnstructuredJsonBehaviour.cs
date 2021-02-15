d93fd12b28e088cf34dd1816de968cff467dd363
jblam <jeremy.blamey@gmail.com>
2021-02-15 11:09:39 +1100

    A really-ugly way of mutating from input to output

--- Multiflash.Tests/UnstructuredJsonBehaviour.cs
+++ Multiflash.Tests/UnstructuredJsonBehaviour.cs
@@ -11,18 +11,26 @@ namespace JBlam.Multiflash.Tests
     [TestClass]
     public class UnstructuredJsonBehaviour
     {
-        record UnstructuredContent(string U, object Root);
+        record UnstructuredContent(string Fixed, object Template);
         [TestMethod]
         public void CanDeserialiseUnstructured()
         {
-            var json = @"{""u"": ""v"", ""root"":{""x"": ""x""}}";
+            var json = @"{""fixed"": ""v"", ""template"":{""x"": ""x""}}";
             var doc = JsonSerializer.Deserialize<UnstructuredContent>(json, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
-            Assert.AreEqual(doc.U, "v");
-            var unstructured = doc.Root;
+            Assert.AreEqual(doc.Fixed, "v");
+            var unstructured = doc.Template;
             Assert.IsInstanceOfType(unstructured, typeof(JsonElement));
             Assert.IsTrue(((JsonElement)unstructured).TryGetProperty("x", out var prop));
             Assert.AreEqual(JsonValueKind.String, prop.ValueKind);
             Assert.AreEqual("x", prop.GetString());
+        }
+        [TestMethod]
+        public void CanMutateJsonDocument()
+        {
+            var json = @"{""fixed"": ""1234"", ""template"": {""parameter"": ""{{PARAMETER-NAME}}""}}";
+            var doc = JsonSerializer.Deserialize<UnstructuredContent>(json, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+            var output = JsonSerializer.Serialize(doc.Template).Replace("{{PARAMETER-NAME}}", "value");
+            Assert.AreEqual(@"{""parameter"":""value""}", output);
         }
     }
 }
