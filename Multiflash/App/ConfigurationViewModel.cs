97982669e3bf3c006449eec3bb742aa469bc8868
jblam <jeremy.blamey@gmail.com>
2021-02-18 18:50:10 +1100

    The config is now sendable

--- Multiflash/App/ConfigurationViewModel.cs
+++ Multiflash/App/ConfigurationViewModel.cs
@@ -19,7 +19,16 @@ namespace JBlam.Multiflash.App
             Connection = SerialConnection.Open(comPort, 115200);
             GetStatus = Command.Create(async () => StatusValue = await Connection.Prompt("?", true));
             Verifications = binarySet.Verifications.Select(v => new VerificationViewModel(v, Connection)).ToList();
-            Parameters = binarySet.ConfigTemplate.Parameters?.Select(p => new ParameterViewModel(p)).ToList() as IReadOnlyCollection<ParameterViewModel> ?? Array.Empty<ParameterViewModel>();
+            Parameters = binarySet.ConfigTemplate?.Parameters.Select(p => new ParameterViewModel(p)).ToList() as IReadOnlyCollection<ParameterViewModel>
+                ?? Array.Empty<ParameterViewModel>();
+            CommitConfig = Command.Create(
+                () => Connection.Prompt($"{binarySet.ConfigTemplate!.SerialCommand} {binarySet.ConfigTemplate!.Build(GetValue)}"),
+                () => (binarySet.ConfigTemplate?.SerialCommand) != null && Parameters.All(p => p.Value != null));
+
+            foreach (var parameter in Parameters)
+            {
+                parameter.PropertyChanged += (sender, args) => CommitConfig.RaiseCanExecuteChanged();
+            }
         }
 
         string? statusValue;
@@ -28,6 +37,7 @@ namespace JBlam.Multiflash.App
 
         public IReadOnlyCollection<VerificationViewModel> Verifications { get; }
         public IReadOnlyCollection<ParameterViewModel> Parameters { get; }
+        public ICommand CommitConfig { get; }
 
         SerialConnection Connection { get; }
         public ICommand GetStatus { get; }
@@ -42,5 +52,7 @@ namespace JBlam.Multiflash.App
         }
 
         public ObservableCollection<Message> SerialContent => Connection.Output;
+
+        internal string? GetValue(Parameter p) => Parameters.FirstOrDefault(vm => vm.Parameter.Identifier == p.Identifier)?.Value;
     }
 }
--- Multiflash/Flashing/ConfigTemplate.cs
+++ Multiflash/Flashing/ConfigTemplate.cs
@@ -33,14 +33,14 @@ namespace JBlam.Multiflash
         /// </summary>
         /// <param name="lookup">
         /// A function which returns a string value for the specified parameter. A <see langword="null"/>
-        /// return will cause the <see cref="Build(Func{Parameter, string})"/> to throw.
+        /// return will cause the <see cref="Build(Func{Parameter, string?})"/> to throw.
         /// </param>
         /// <returns>The constructed JSON</returns>
         /// <remarks>
         /// Only JSON-string parameter values are supported. Neither the supplied values nor the constructed
         /// output are checked for syntax or structure.
         /// </remarks>
-        public string Build(Func<Parameter, string> lookup)
+        public string Build(Func<Parameter, string?> lookup)
         {
             var templateString = JsonSerializer.Serialize(Template);
             foreach (var parameter in Parameters)
